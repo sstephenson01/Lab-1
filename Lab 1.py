dd4bde6129543c78bd7aa3ff74dfb217fb3878d4
Sarah Stephenson <123026466+sstephenson01@users.noreply.github.com>
2023-01-25 15:53:57 -0500

    Add files via upload

---
+++ Lab 1.py
@@ -0,0 +1,42 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+#The authors name is : Sarah
+1+*3
+SyntaxError: invalid syntax
+1+2*3
+7
+(1+2)*3
+9
+7/3
+2.3333333333333335
+7//3
+2
+7%3
+1
+22/4
+5.5
+22//4
+5
+22%4
+2
+3**2
+9
+2**3
+8
+(4+3)//2
+3
+6%2
+0
+1+2*3**4
+163
+print("2+2", 2+2)
+2+2 4
+2+2 4
+SyntaxError: invalid syntax
+prinmt("2+2=", 2+2)
+Traceback (most recent call last):
+  File "<pyshell#17>", line 1, in <module>
+    prinmt("2+2=", 2+2)
+NameError: name 'prinmt' is not defined. Did you mean: 'print'?
+print("2+2=", 2+2)
+2+2= 4
